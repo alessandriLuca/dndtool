02659eff084730821f26cd70349ae6d44b3b76be
lucastormreig <alessandri.luca1991@gmail.com>
2023-11-13 22:14:42 -0500

    automatic push

---
+++ src/Preview.jsx
@@ -0,0 +1,86 @@
+// Preview.js
+
+import React from 'react';
+
+function Preview({ yamlData, imageUrl }) {
+  return (
+    <div className="preview-container">
+      <div className="left-section">
+        <h2 className="title">{yamlData.title}</h2>
+        {imageUrl && <img src={imageUrl} alt={yamlData.title} className="character-image" />}
+        <div className="character-stats">
+          <h3>Stats</h3>
+          <hr />
+          <p><strong>Armor Class:</strong> {yamlData.armor_class}</p>
+          <p><strong>Speed:</strong> {yamlData.speed}</p>
+          <p><strong>Max Hit Points:</strong> {yamlData.max_hit_points}</p>
+          <hr />
+          <div className="attributes-container">
+            {['strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma'].map((attr) => (
+              <div key={attr} className="attribute">
+                <strong>{attr.substring(0, 3).toUpperCase()}</strong>
+                <p>{yamlData[attr]}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Special Actions Section */}
+        {yamlData.special_actions && (
+          <div className="special-actions-container">
+            <h3>Special Actions</h3>
+            <hr />
+            {yamlData.special_actions.map((category, index) => (
+              <div key={index}>
+                <h4>{category.category}</h4>
+                {category.actions.map((action, subIndex) => (
+                  <div key={subIndex} className="special-action">
+                    <strong>{action.name}</strong>
+                    <p>{action.description}</p>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="right-section">
+        <h3>Details</h3>
+        <hr />
+        <p>{yamlData.subtitle}</p>
+        <p><strong>Challenge Rating:</strong> {yamlData.challenge_rating}</p>
+        <p><strong>Experience Points:</strong> {yamlData.experience_points}</p>
+        <hr />
+        {yamlData.actions && (
+          <div className="actions-container">
+            <h3>Actions</h3>
+            <hr />
+            {yamlData.actions.map((action, index) => (
+              <div key={index} className="action">
+                <strong>{action.name}</strong>
+                <p>{action.description}</p>
+                <p><strong>Attack Bonus:</strong> {action.attack_bonus}</p>
+                <p><strong>Reach:</strong> {action.reach}</p>
+                <p><strong>Damage:</strong> {action.damage}</p>
+              </div>
+            ))}
+          </div>
+        )}
+        {yamlData.loot && (
+          <div className="loot-container">
+            <h3>Loot</h3>
+            <hr />
+            {yamlData.loot.map((loot, index) => (
+              <div key={index} className="loot">
+                <strong>{loot.name}</strong>
+                <p>{loot.description}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Preview;
--- src/working/App.jsx
+++ src/working/App.jsx
@@ -1,142 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import YAML from 'js-yaml';
-import html2pdf from 'html2pdf.js';
+import html2canvas from 'html2canvas';
 
-const defaultYaml = `title: Goblin
-subtitle: Small humanoid (goblinoid), neutral evil
-challenge_rating: 1/4
-experience_points: 50
-armor_class: 15 (Leather armor, shield)
-max_hit_points: 7 (2d6)
-speed: 30 ft.
-strength: 8 (-1)
-dexterity: 14 (+2)
-constitution: 10 (+0)
-intelligence: 10 (+0)
-wisdom: 8 (-1)
-charisma: 8 (-1)
+import { defaultYaml } from './character.js';
 
-actions:
-  - name: Scimitar
-    description: "Slashes with a sharp scimitar."
-    attack_bonus: +4
-    reach: "5 ft."
-    damage: "5 (1d6 + 2) slashing damage."
+// Ora puoi utilizzare defaultYaml nei tuoi componenti React
+//console.log(defaultYaml);
 
-  - name: Shortbow
-    description: "Shoots a shortbow arrow."
-    attack_bonus: +4
-    range: "80/320 ft."
-    damage: "5 (1d6 + 2) piercing damage."
-
-loot:
-  - name: Gold Pouch
-    description: "A small pouch containing 10 gold pieces."
-  - name: Rusty Key
-    description: "An old key with a rusty appearance."
-`;
-
-function Preview({ yamlData, imageUrl }) {
-  return (
-    <div className="preview-container">
-      <div className="image-title-container">
-        <h2>{yamlData.title}</h2>
-        {imageUrl && <img src={imageUrl} alt={yamlData.title} />}
-      </div>
-      <div className="info-container">
-        <p>{yamlData.subtitle}</p>
-        <p><strong>Challenge Rating:</strong> {yamlData.challenge_rating}</p>
-        <p><strong>Experience Points:</strong> {yamlData.experience_points}</p>
-        <p><strong>Armor Class:</strong> {yamlData.armor_class}</p>
-        <p><strong>Speed:</strong> {yamlData.speed}</p>
-        <p><strong>Max Hit Points:</strong> {yamlData.max_hit_points}</p>
-
-        <div className="attributes-container">
-          <div className="attribute">
-            <div className="attribute-name"><strong>Strength</strong></div>
-            <div className="attribute-value">{yamlData.strength}</div>
-          </div>
-          <div className="attribute">
-            <div className="attribute-name"><strong>Dexterity</strong></div>
-            <div className="attribute-value">{yamlData.dexterity}</div>
-          </div>
-          <div className="attribute">
-            <div className="attribute-name"><strong>Constitution</strong></div>
-            <div className="attribute-value">{yamlData.constitution}</div>
-          </div>
-          <div className="attribute">
-            <div className="attribute-name"><strong>Intelligence</strong></div>
-            <div className="attribute-value">{yamlData.intelligence}</div>
-          </div>
-          <div className="attribute">
-            <div className="attribute-name"><strong>Wisdom</strong></div>
-            <div className="attribute-value">{yamlData.wisdom}</div>
-          </div>
-          <div className="attribute">
-            <div className="attribute-name"><strong>Charisma</strong></div>
-            <div className="attribute-value">{yamlData.charisma}</div>
-          </div>
-        </div>
-      </div>
-
-      {yamlData.actions && (
-        <div className="actions-container">
-          <h3>Actions</h3>
-          {yamlData.actions.map((action, index) => (
-            <div key={index} className="action">
-              <p><strong>{action.name}</strong></p>
-              <p>{action.description}</p>
-              <p><strong>Attack Bonus:</strong> {action.attack_bonus}</p>
-              <p><strong>Reach:</strong> {action.reach}</p>
-              <p><strong>Damage:</strong> {action.damage}</p>
-              {index < yamlData.actions.length - 1 && <hr />}
-            </div>
-          ))}
-        </div>
-      )}
-
-      {yamlData.loot && (
-        <div className="loot-container">
-          <h3>Loot</h3>
-          {yamlData.loot.map((loot, index) => (
-            <div key={index} className="loot">
-              <p><strong>{loot.name}</strong></p>
-              <p>{loot.description}</p>
-              {index < yamlData.loot.length - 1 && <hr />}
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
-}
-
-
-
-
-
-
-
-
+import Preview from './Preview'; // Importa il componente Preview
 
 
 
 function App() {
   const [inputText, setInputText] = useState(defaultYaml);
-  const [yamlData, setYamlData] = useState(null);
-  const [imageUrl, setImageUrl] = useState(null);
+  const [yamlData, setYamlData] = useState({});
+  const [imageUrl, setImageUrl] = useState('');
+  const previewRef = useRef(null);
+
+  useEffect(() => {
+    try {
+      const parsedYaml = YAML.load(defaultYaml);
+      setYamlData(parsedYaml);
+    } catch (error) {
+      console.error('Error parsing YAML:', error);
+      setYamlData({});
+    }
+  }, []);
 
   const handleInputChange = (event) => {
     const newText = event.target.value;
     setInputText(newText);
-
     try {
       const parsedYaml = YAML.load(newText);
       setYamlData(parsedYaml);
     } catch (error) {
-      console.error('Errore nel parsing YAML:', error);
-      setYamlData(null);
+      console.error('Error parsing YAML:', error);
+      setYamlData({});
     }
   };
 
@@ -148,39 +48,61 @@ function App() {
     }
   };
 
-  useEffect(() => {
-    try {
-      const parsedYaml = YAML.load(defaultYaml);
-      setYamlData(parsedYaml);
-    } catch (error) {
-      console.error('Errore nel parsing YAML:', error);
-      setYamlData(null);
-    }
-  }, []);
+const handleDownloadAsPng = () => {
+  const element = previewRef.current; // Reference to the preview container
+  if (element) {
+    html2canvas(element, {
+      onclone: (document) => {
+        document.querySelector('.preview-container').style.transform = 'none';
+      },
+    }).then((canvas) => {
+      // Create a data URL
+      const image = canvas.toDataURL('image/png');
+      // Create a link to download the image
+      const link = document.createElement('a');
+      link.href = image;
+      link.download = 'preview.png';
+      // Append to the DOM, click it, and then remove it
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+  }
+};
 
   return (
     <div className="app-container">
       <div className="text-input-container">
         <textarea
           rows="16"
-          placeholder="Inserisci del testo YAML qui..."
+          placeholder="Insert YAML text here..."
           value={inputText}
           onChange={handleInputChange}
           className="text-input"
         />
       </div>
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleImageChange}
-        className="image-input"
-      />
-      {yamlData && imageUrl && (
-        <div className="preview-container background-container">
+      <div className="input-container">
+        <label htmlFor="file-upload" className="custom-file-upload">
+          Upload Image
+        </label>
+        <input
+          id="file-upload"
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          style={{ display: 'none' }}
+        />
+      <button onClick={handleDownloadAsPng} className="custom-button">
+        Download Preview as PNG
+      </button>
+      </div>
+      {yamlData && (
+        <div ref={previewRef} className="preview-container">
           <Preview yamlData={yamlData} imageUrl={imageUrl} />
         </div>
       )}
     </div>
   );
 }
+
 export default App;
