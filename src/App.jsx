01559eaf65399ee0ab61ad6a4b74c27f30b126ca
lucastormreig <alessandri.luca1991@gmail.com>
2023-11-15 18:35:37 -0500

    automatic push

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,14 @@ function App() {
             <span>☰</span>
           </div>
           <ul className={isMenuOpen ? "nav-links showNav" : "nav-links"}>
-            <li><Link to="/" onClick={toggleMenu}>Yaml To Character Sheet</Link></li>
-            <li><Link to="/secondpage" onClick={toggleMenu}>Seconda Pagina</Link></li>
+            <li><Link to="/yamlTool" onClick={toggleMenu}>Yaml To Character Sheet</Link></li>
+            <li><Link to="/secondPage" onClick={toggleMenu}>Seconda Pagina</Link></li>
           </ul>
         </nav>
         <main>
           <Routes>
-            <Route path="/" element={<YamlTool />} />
-            <Route path="/secondpage" element={<SecondPage />} />
+            <Route path="/yamlTool" element={<YamlTool />} />
+            <Route path="/secondPage" element={<SecondPage />} />
           </Routes>
         </main>
       </div>
