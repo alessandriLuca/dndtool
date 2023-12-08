8c780be7212b21a03badbdb7d51fc1c5f1132b65
lucastormreig <alessandri.luca1991@gmail.com>
2023-12-08 00:48:02 -0500

    automatic push

--- src/SecondPage.jsx
+++ src/SecondPage.jsx
@@ -6,12 +6,20 @@ function SecondPage() {
   const [items, setItems] = useState([]);
   const [currentItem, setCurrentItem] = useState({});
   const [error, setError] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [rarity, setRarity] = useState(0);
 
   useEffect(() => {
     loadDefaultDatabase();
   }, []);
 
+  const showMessage = (error, success) => {
+    setError(error);
+    setSuccessMessage(success);
+  };
+
   const loadDefaultDatabase = async () => {
+    showMessage('', '');
     try {
       const response = await fetch('./database.txt');
       if (!response.ok) {
@@ -19,72 +27,80 @@ function SecondPage() {
       }
       const text = await response.text();
       processDatabaseText(text);
-    } catch (error) {
-      setError(error.message);
+      showMessage('', 'Default database loaded successfully!');
+    } catch (err) {
+      showMessage(err.message, '');
     }
   };
 
   const handleRandomItem = () => {
+    showMessage('', '');
     if (items.length === 0) {
-      setError('No items available in the database');
+      showMessage('No items available in the database', '');
       return;
     }
-
-    // Genera un numero casuale per selezionare un elemento casuale dall'array degli oggetti
-    const randomIndex = Math.floor(Math.random() * items.length);
-    const selectedItem = items[randomIndex];
-    setCurrentItem(selectedItem);
+    const rarityValue = parseInt(rarity);
+    if (rarityValue >= 1 && rarityValue <= 20) {
+      const filteredItems = items.filter(item => parseInt(item.Rarity) === rarityValue);
+      if (filteredItems.length === 0) {
+        showMessage(`No items with rarity ${rarityValue}`, '');
+        return;
+      }
+      const randomIndex = Math.floor(Math.random() * filteredItems.length);
+      setCurrentItem(filteredItems[randomIndex]);
+    } else {
+      const randomIndex = Math.floor(Math.random() * items.length);
+      setCurrentItem(items[randomIndex]);
+    }
+    showMessage('', 'Random item selected successfully!');
   };
 
   const handleCustomDatabaseUpload = async (event) => {
+    showMessage('', '');
     const file = event.target.files[0];
     if (file) {
       const reader = new FileReader();
-      reader.onload = async (e) => {
+      reader.onload = (e) => {
         const text = e.target.result;
         processDatabaseText(text);
+      };
+      reader.onerror = (err) => {
+        showMessage(err.message, '');
       };
       reader.readAsText(file);
     }
   };
-  const handleMagicDatasetLoad = async () => {
-    try {
-      const response = await fetch('./magicDataset.txt'); // Assicurati che il percorso sia corretto
-      if (!response.ok) {
-        throw new Error('Unable to load the magic dataset');
-      }
-      const text = await response.text();
-      processDatabaseText(text);
-    } catch (error) {
-      setError(error.message);
-    }
-  };
+
   const processDatabaseText = (text) => {
     const lines = text.split('\n').filter(line => line.trim());
     if (lines.length === 0) {
-      setError('File is empty or not valid');
+      showMessage('File is empty or not valid', '');
       return;
     }
 
     const newHeaders = lines[0].split('|');
     if (newHeaders.length < 2) {
-      setError('File format is not valid. Make sure the file is "|" separated.');
+      showMessage('File format is not valid. Make sure the file is "|" separated.', '');
       return;
     }
 
     const newItems = lines.slice(1).map(line => {
       const values = line.split('|');
-      const item = {};
-      newHeaders.forEach((header, index) => {
-        item[header] = values[index];
-      });
-      return item;
+      return newHeaders.reduce((item, header, index) => {
+        item[header] = values[index] || '';
+        return item;
+      }, {});
     });
 
     setHeaders(newHeaders);
     setItems(newItems);
-    setCurrentItem({}); // Resetta l'oggetto corrente
-    setError('');
+    setCurrentItem({});
+    showMessage('', 'Database processed successfully!');
+  };
+
+  const handleRarityChange = (event) => {
+    const value = event.target.value;
+    setRarity(value);
   };
 
   return (
@@ -92,7 +108,6 @@ function SecondPage() {
       <div className="button-container">
         <button onClick={handleRandomItem}>Random Item</button>
         <button onClick={loadDefaultDatabase}>Common Database</button>
-        <button onClick={handleMagicDatasetLoad}>Load Magic Database</button> {/* Aggiunto il nuovo bottone */}
         <label htmlFor="file-upload" className="custom-file-upload">
           Load Custom Database
         </label>
@@ -103,10 +118,24 @@ function SecondPage() {
           accept=".txt"
           style={{ display: 'none' }}
         />
-        {error && <div className="error-message">{error}</div>}
+      <div className="fancy-input-container">
+  <label htmlFor="rarity-input" className="fancy-label">Rarity:</label>
+  <input
+    id="rarity-input"
+    type="number"
+    value={rarity}
+    onChange={handleRarityChange}
+    min="0"
+    max="20"
+    step="1"
+    className="fancy-input"
+  />
+</div>
       </div>
-      <div className="table-container">
-        {headers.length > 0 && Object.keys(currentItem).length > 0 && (
+      {error && <div className="error-message">{error}</div>}
+      {successMessage && <div className="success-message">{successMessage}</div>}
+      {headers.length > 0 && Object.keys(currentItem).length > 0 && (
+        <div className="table-container">
           <table>
             <thead>
               <tr>
@@ -117,14 +146,14 @@ function SecondPage() {
             </thead>
             <tbody>
               <tr>
-                {headers.map((header, index) => (
-                  <td key={`item-${index}`}>{currentItem[header]}</td>
+                {headers.map((header) => (
+                  <td key={`item-${header}`}>{currentItem[header]}</td>
                 ))}
               </tr>
             </tbody>
           </table>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
